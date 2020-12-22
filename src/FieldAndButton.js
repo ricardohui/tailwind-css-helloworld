3b640704fa1e02d87e6c40e294b8a05450395dc8
Ricardo Hui <rcdhui@gmail.com>
2020-12-22 22:52:03 +0800

    add button handler

--- src/FieldAndButton.js
+++ src/FieldAndButton.js
@@ -6,7 +6,12 @@ export default function FieldAndButton() {
           className="border border-transparent p-3 w-56 shadow-lg mr-5 rounded-sm focus:ring-8 focus:ring-purple-600  focus:outline-none focus:border-transparent"
           placeholder="Your email"
         ></input>
-        <button className="bg-purple-600 text-white p-3 rounded-lg hover:bg-purple-700 focus:outline-none focus:ring-8 focus:ring-purple-100 focus:ring-opacity-50">
+        <button
+          className=" bg-primary-600 text-white p-3 rounded-lg hover:bg-purple-700 focus:outline-none focus:ring-8 focus:ring-purple-100 focus:ring-opacity-90"
+          onClick={(e) => {
+            e.preventDefault();
+          }}
+        >
           Sign Up
         </button>
       </form>
