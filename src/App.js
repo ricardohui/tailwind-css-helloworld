c0ab9437f25bf86004e20be56916579fd712bf9c
Ricardo Hui <rcdhui@gmail.com>
2020-12-22 18:12:47 +0800

    change App.js to be a entry point

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import logo from "./logo.svg";
-
+import ResponsiveCard from "./ResponsiveCard";
+import FieldAndButton from "./FieldAndButton";
 function App() {
   return (
-    <div className="max-w-md mx-auto bg-white rounded-xl md:max-w-xl overflow-hidden shadow-xl">
-      <div className="md:flex">
-        <div className="md:flex">
-          <img
-            className="h-48 w-full object-cover md:w-48"
-            src={logo}
-            alt="ChitChat Logo"
-          />
-        </div>
-        <div className="p-7">
-          <div className="uppercase text-indigo-500 font-semibold text-sm">
-            Case Study
-          </div>
-          <a className="block mt-1 text-lg leading-tight font-medium text-black hover:underline ">
-            Find customers for your new business
-          </a>
-          <p className="mt-2 text-gray-500">You have a new message!</p>
-        </div>
-      </div>
+    <div className="flex flex-wrap justify-center align-middle p-24">
+      <ResponsiveCard />
+      <div className="p-64"></div>
+      <FieldAndButton />
     </div>
   );
 }
