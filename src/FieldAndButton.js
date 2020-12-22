3c2f20bc086e94b21349df2371299f1707ec1c6b
Ricardo Hui <rcdhui@gmail.com>
2020-12-22 22:51:52 +0800

    add padding

--- src/FieldAndButton.js
+++ src/FieldAndButton.js
@@ -1,6 +1,6 @@
 export default function FieldAndButton() {
   return (
-    <div className="">
+    <div className="p-4">
       <form className="">
         <input
           className="border border-transparent p-3 w-56 shadow-lg mr-5 rounded-sm focus:ring-8 focus:ring-purple-600  focus:outline-none focus:border-transparent"
